2c4194bef9d70e792d2ace56d980433bf2f9a786
kshitij-hz <kshitij.hz@gmail.com>
2016-06-27 21:43:37 +0530

    Add files via upload

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -50,6 +50,7 @@ class AuthController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|max:255',
+            'contact' => 'required|min:10|max:10|unique:users',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
@@ -65,6 +66,7 @@ class AuthController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'contact' => $data['contact'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
