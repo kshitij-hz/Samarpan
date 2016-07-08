6c2abfd2a111b7a8af7287f4456247029050ef15
kshitij-hz <kshitij.hz@gmail.com>
2016-07-09 05:19:26 +0530

    Add files via upload

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\Http\Requests;
+use App\User;
+// use App\Verification;
+use Illuminate\Support\Facades\Input;
+use Mail;
 
 class PageController extends Controller
 {
@@ -23,8 +27,24 @@ class PageController extends Controller
     	}
     }
 
-    public function contact() {
+    public function contact(Request $request) {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required'
+        ]);
+          $name = Input::get('name');
+          $email = Input::get('email');
+          $msg = Input::get('message');
+            
+        $user = User:: where('type','0')->first();
         
+        Mail::send('admin.contact', array('name' => $name,'email' => $email,'msg' => $msg), function($message) use($user) {
+            $message->to($user->email, $user->name)->subject('Samarpan Contact');
+        });
+        
+        return redirect('/')->with('contact_result', 'completed');
+
     }
 
     public function account() {
