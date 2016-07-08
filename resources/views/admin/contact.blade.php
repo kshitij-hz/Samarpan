bf051943d244257f18a0774dd30e9f18ae0a5cf9
kshitij-hz <kshitij.hz@gmail.com>
2016-07-09 05:20:39 +0530

    Add files via upload

---
+++ resources/views/admin/contact.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        
+        <div>
+        <b>
+        Name : {{{ $name }}}
+        <br>
+        Email: {{{ $email}}}
+        <br>
+        Message : {{{ $msg }}}
+        <br>
+
+        </b>
+        </div>
+    </body>
+</html>
